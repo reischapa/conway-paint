9c09e0e3ed2ec921a450eb2c952132627b1f90df
João Chapa <reischapa@gmail.com>
2020-12-03 22:16:34 +0000

    feat: version with paint?

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function getCanvas() {
-    return document.getElementById("canvas");
+  return document.getElementById("canvas");
 }
 
 function getCtx() {
-    return getCanvas().getContext('2d');
+  return getCanvas().getContext('2d');
 }
 
-function clear() {
-    const ctx = getCtx();
-
-    ctx.clearRect(0, 0, 1000, 1000);
-    ctx.beginPath();
+function getBrushTypeSelect() {
+  return document.getElementById("brushType");
 }
 
 const boardWidth = 1000;
@@ -22,11 +19,10 @@ const heightInCells = 100;
 const cellWidth = boardWidth/widthInCells;
 const cellHeight = boardHeight/heightInCells;
 
-class LinkedListNode {
-  constructor() {
-    this.visited = false;
-    this.painted = false;
+const brushRadius = 3 * cellWidth;
 
+class LinkedNode {
+  constructor() {
     this.fill = false;
     this.fillNext = false;
 
@@ -42,31 +38,25 @@ class LinkedListNode {
     this.yIndex = 0;
   }
 
-  paint() {
-    const ctx = getCtx();
+  paint(pixels) {
+    const color = this.fillNext ? 255 : 0;
 
-    ctx.rect(this.x, this.y, cellWidth, cellHeight);
-    ctx.fill();
-  }
+    const fillCellLine = (startY) => {
+      let startX = (startY * boardWidth) + this.x;
 
-  nextVisited() {
-    if (this.right && this.right.visited) {
-      return this.right;
+      for (let i = 0; i < cellWidth; i++) {
+        const index = startX + i;
+
+        pixels[index * 4] = color;
+        pixels[(index * 4) + 1] = color;
+        pixels[(index * 4) + 2] = color;
+        pixels[(index * 4) + 3] = 255;
+      }
     }
 
-    if (this.down && this.down.visited) {
-      return this.down;
+    for (let startY = 0; startY < cellHeight; startY++) {
+      fillCellLine(this.y + startY);
     }
-
-    if (this.left && this.left.visited) {
-      return this.left;
-    }
-
-    if (this.up && this.up.visited) {
-      return this.up;
-    }
-
-    return null;
   }
 }
 
@@ -76,7 +66,7 @@ class LinkedList {
     this.height = height;
 
     const totalLength = width * height;
-    this.nodes = Array(totalLength).fill().map(() => new LinkedListNode());
+    this.nodes = Array(totalLength).fill().map(() => new LinkedNode());
 
     for (let j = 0; j < height; j++) {
       for (let i = 0; i < width; i++) {
@@ -90,13 +80,13 @@ class LinkedList {
         let upIndex = ((j - 1) * height) + i;
         let downIndex = ((j + 1) * height) + i;
 
-        if (leftIndex >= 0 && (leftIndex % width) != (width - 1)) {
+        if (leftIndex >= 0 && (leftIndex % width) !== (width - 1)) {
           node.left = this.nodes[leftIndex];
         } else {
           node.left = this.nodes[j * height + width - 1];
         }
 
-        if (rightIndex < totalLength && (rightIndex % width) != 0) {
+        if (rightIndex < totalLength && (rightIndex % width) !== 0) {
           node.right = this.nodes[rightIndex];
         } else {
           node.right = this.nodes[j * height];
@@ -169,56 +159,23 @@ function getNeigh(node) {
   return neigh;
 }
 
-
-const leftShip = [
-  [0, 0],
-  [0, 1],
-  [0, 2],
-  [1, 2],
-  [2, 1]
-];
-
-const rightShip = [
-  [2, 0],
-  [2, 1],
-  [2, 2],
-  [1, 2],
-  [0, 1]
-];
-
 let list;
 let filledCells;
 
 filledCells = [];
 
-/**
-for (let i = 0; i < 100; i++) {
-  const randomX = Math.floor(Math.random() * 80);
-  const randomY = Math.floor(Math.random() * 80);
+function setup(options = {resetList: true, filledCells: []} ) {
+  filledCells = options.filledCells;
 
-  const ship = !!Number(Math.round(Math.random())) ? leftShip : rightShip;
-
-  const newElem = ship.map(p => {
-    return [
-      p[0] + randomX,
-      p[1] + randomY
-    ];
-  });
-
-  filledCells.push(...newElem);
-}
-**/
-
-function setup(options = {resetGrid: true} ) {
-  if (options.resetGrid) {
+  if (options.resetList) {
     list = new LinkedList(widthInCells, heightInCells);
   }
-  
-  filledCells.forEach(point => {
+
+  for (const point of filledCells) {
     const node = list.getNode(point[0], point[1]);
 
     node.fillNext = true;
-  })
+  }
 
   for (const node of list.nodes) {
     node.visited = true;
@@ -228,9 +185,7 @@ function setup(options = {resetGrid: true} ) {
 }
 
 function calculateNextStep() {
-  let node = list.getNode(0,0);
-
-  while (true) {
+  for (const node of list.nodes) {
     node.visited = true;
     node.fillNext = false;
 
@@ -245,52 +200,131 @@ function calculateNextStep() {
         node.fillNext = false;
       }
     } else {
-      if (neigh === 3) {
-        node.fillNext = true;
-      } else {
-        node.fillNext = false;
-      }
-    }
-
-    let found = false;
-
-    ['left', 'up', 'right', 'down'].forEach(d => {
-        if (!found && !!node[d] && !node[d].visited) {
-          node = node[d];
-          found = true;
-        }
-    });
-
-    if (!found) {
-      break;
+      node.fillNext = neigh === 3;
     }
   }
 }
 
 function paintNextStep() {
   const ctx = getCtx();
-  const canvas = getCanvas();
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
+  const imageData = ctx.getImageData(0, 0, boardWidth, boardHeight);
 
-  canvas.width = canvas.width;
+  const pixels = imageData.data;
 
-  ctx.fillStyle = "navy";
+  for (const current of list.nodes) {
+    current.paint(pixels);
 
-  let current = list.getNode(0,0);
-
-  while(current) {
     current.fill = current.fillNext;
-
-    if (current.fill) {
-      current.paint();
-    } 
-
-    current.visited = false;
-
-    current = current.nextVisited()
   }
+
+  ctx.putImageData(imageData, 0, 0)
+
+}
+
+function drawSquare(nodes, targetNodes, x, y) {
+  for (const n of nodes) {
+    const xIndex = Math.floor(x / cellWidth);
+    const yIndex = Math.floor(y / cellHeight);
+
+    if (n.xIndex < xIndex - 1 || n.xIndex >= (xIndex + 2) || n.yIndex < yIndex - 1 || n.yIndex >= (yIndex + 2)) {
+        continue;
+    }
+
+    targetNodes.push(n);
+  }
+}
+
+function drawCircle(nodes, targetNodes, x, y) {
+  for (const n of nodes) {
+    const dist = Math.sqrt(((n.x + cellWidth/2) - x) ** 2 + ((n.y + cellHeight/2) - y) ** 2);
+    if (dist < brushRadius) {
+      targetNodes.push(n);
+    }
+  }
+}
+
+function drawLeftShip(nodes, targetNodes, x, y) {
+  for (const n of nodes) {
+    const xIndex = Math.floor(x / cellWidth);
+    const yIndex = Math.floor(y / cellHeight);
+
+    if (n.xIndex < xIndex - 1 || n.xIndex >= (xIndex + 2) || n.yIndex < yIndex - 1 || n.yIndex >= (yIndex + 2)) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex && n.yIndex === yIndex) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex && n.yIndex === yIndex - 1) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex + 1 && n.yIndex === yIndex - 1) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex + 1 && n.yIndex === yIndex + 1) {
+      continue;
+    }
+
+    targetNodes.push(n);
+  }
+}
+
+function drawRightShip(nodes, targetNodes, x, y) {
+  for (const n of nodes) {
+    const xIndex = Math.floor(x / cellWidth);
+    const yIndex = Math.floor(y / cellHeight);
+
+    if (n.xIndex < xIndex - 1 || n.xIndex >= (xIndex + 2) || n.yIndex < yIndex - 1 || n.yIndex >= (yIndex + 2)) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex && n.yIndex === yIndex) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex && n.yIndex === yIndex - 1) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex - 1 && n.yIndex === yIndex - 1) {
+      continue;
+    }
+
+    if (n.xIndex === xIndex - 1 && n.yIndex === yIndex + 1) {
+      continue;
+    }
+
+    targetNodes.push(n);
+  }
+}
+
+let selectedBrush;
+
+const brushes = {
+  circle: drawCircle,
+  leftShip: drawLeftShip,
+  rightShip: drawRightShip,
+  square: drawSquare
+}
+
+function draw(x, y) {
+  let targetNodes = [];
+
+  const brush = brushes[selectedBrush] || drawCircle;
+
+  brush(list.nodes, targetNodes, x, y);
+
+  filledCells = [];
+
+  for (const targetNode of targetNodes) {
+    filledCells.push([targetNode.xIndex, targetNode.yIndex]);
+  }
+
+  setup({resetList: false, filledCells});
 }
 
 window.onload = function () {
@@ -301,31 +335,50 @@ window.onload = function () {
 
   setup();
 
+  const canvas = getCanvas();
   const playButton = document.getElementById("button-play");
   const stepButton = document.getElementById("button-step");
   const resetButton = document.getElementById("button-reset");
+  const brushTypeSelect = getBrushTypeSelect();
 
   let running = false;
+  let drawing = false;
+
+  let lastMouseOffsetX;
+  let lastMouseOffsetY;
 
   function run() {
     if (running) {
       step();
-    }
-
-    if (running) {
       playButton.firstChild.data = "Pause";
       stepButton.disabled = true;
       resetButton.disabled = true;
+      brushTypeSelect.disabled = true;
+
+      canvas.style.cursor = 'wait';
     } else {
       playButton.firstChild.data = "Play";
       stepButton.disabled = false;
       resetButton.disabled = false;
+      brushTypeSelect.disabled = false;
+
+      canvas.style.cursor = 'crosshair';
     }
 
     setTimeout(run, 16);
   }
 
   run();
+
+  function runPaint() {
+    if (drawing) {
+      draw(lastMouseOffsetX, lastMouseOffsetY);
+    }
+
+    setTimeout(runPaint, 16);
+  }
+
+  runPaint();
 
   playButton.addEventListener('click', evt => {
     running = !running;
@@ -342,36 +395,26 @@ window.onload = function () {
 
     setup();
   });
+
   
-  getCanvas().addEventListener('click', evt => {
-    const x = evt.offsetX;
-    const y = evt.offsetY;
-
-    let targetNode;
-
-    for (const n of list.nodes) {
-      if (
-        (x >= n.x && x <= (n.x + cellWidth)) &&
-        (y >= n.y && y <= (n.y + cellHeight))
-      ) {
-        targetNode = n;
-        break;
-      }
+  canvas.addEventListener('mousedown', evt => {
+    if (!running) {
+      drawing = true;
     }
-
-    const ship = !!Number(Math.round(Math.random())) ? leftShip : rightShip;
-
-    const newElem = ship.map(p => {
-      return [
-        p[0] + targetNode.xIndex,
-        p[1] + targetNode.yIndex 
-      ];
-    });
-
-    filledCells = [];
-
-    filledCells.push(...newElem);
-
-    setup({resetGrid: false});
   });
+
+  canvas.addEventListener('mouseup', evt => {
+    if (!running) {
+      drawing = false;
+    }
+  });
+
+  canvas.addEventListener('mousemove', evt => {
+    lastMouseOffsetX = evt.offsetX;
+    lastMouseOffsetY = evt.offsetY;
+  });
+
+  brushTypeSelect.addEventListener('change', evt => {
+      selectedBrush = evt.target.value;
+  })
 }
